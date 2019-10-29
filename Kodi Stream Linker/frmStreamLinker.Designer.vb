0045cd3c2b4ff749fabb7505097364a7dcf4c931
Brian <8048668+diablodelnoche@users.noreply.github.com>
2019-10-29 19:06:38 -0400

    Add files via upload

---
+++ Kodi Stream Linker/frmStreamLinker.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStreamLinker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblProgramDescription = New System.Windows.Forms.Label()
+        Me.lblStreamURL = New System.Windows.Forms.Label()
+        Me.txtStreamURL = New System.Windows.Forms.TextBox()
+        Me.cmdCreateStream = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.cmdClearInput = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblProgramDescription
+        '
+        Me.lblProgramDescription.AutoSize = True
+        Me.lblProgramDescription.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProgramDescription.Location = New System.Drawing.Point(12, 9)
+        Me.lblProgramDescription.Name = "lblProgramDescription"
+        Me.lblProgramDescription.Size = New System.Drawing.Size(691, 18)
+        Me.lblProgramDescription.TabIndex = 0
+        Me.lblProgramDescription.Text = "This Application takes a URL to a media file as input and generates stream files " &
+    "designed for Kodi. "
+        '
+        'lblStreamURL
+        '
+        Me.lblStreamURL.AutoSize = True
+        Me.lblStreamURL.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStreamURL.Location = New System.Drawing.Point(12, 58)
+        Me.lblStreamURL.Name = "lblStreamURL"
+        Me.lblStreamURL.Size = New System.Drawing.Size(160, 18)
+        Me.lblStreamURL.TabIndex = 1
+        Me.lblStreamURL.Text = "Stream URL (http://...):"
+        '
+        'txtStreamURL
+        '
+        Me.txtStreamURL.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStreamURL.Location = New System.Drawing.Point(178, 55)
+        Me.txtStreamURL.Name = "txtStreamURL"
+        Me.txtStreamURL.Size = New System.Drawing.Size(516, 26)
+        Me.txtStreamURL.TabIndex = 2
+        '
+        'cmdCreateStream
+        '
+        Me.cmdCreateStream.Font = New System.Drawing.Font("Franklin Gothic Medium", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdCreateStream.Location = New System.Drawing.Point(492, 87)
+        Me.cmdCreateStream.Name = "cmdCreateStream"
+        Me.cmdCreateStream.Size = New System.Drawing.Size(202, 37)
+        Me.cmdCreateStream.TabIndex = 3
+        Me.cmdCreateStream.Text = "Create Stream File"
+        Me.cmdCreateStream.UseVisualStyleBackColor = True
+        '
+        'cmdClearInput
+        '
+        Me.cmdClearInput.Font = New System.Drawing.Font("Franklin Gothic Medium", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdClearInput.Location = New System.Drawing.Point(284, 87)
+        Me.cmdClearInput.Name = "cmdClearInput"
+        Me.cmdClearInput.Size = New System.Drawing.Size(202, 37)
+        Me.cmdClearInput.TabIndex = 4
+        Me.cmdClearInput.Text = "Reset"
+        Me.cmdClearInput.UseVisualStyleBackColor = True
+        '
+        'frmStreamLinker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 163)
+        Me.Controls.Add(Me.cmdClearInput)
+        Me.Controls.Add(Me.cmdCreateStream)
+        Me.Controls.Add(Me.txtStreamURL)
+        Me.Controls.Add(Me.lblStreamURL)
+        Me.Controls.Add(Me.lblProgramDescription)
+        Me.Name = "frmStreamLinker"
+        Me.Text = "Brian's Kodi Stream Link Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblProgramDescription As Label
+    Friend WithEvents lblStreamURL As Label
+    Friend WithEvents txtStreamURL As TextBox
+    Friend WithEvents cmdCreateStream As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents cmdClearInput As Button
+End Class
---
+++ Kodi Stream Linker/frmStreamLinker.vb
@@ -0,0 +1,74 @@
+﻿Imports System.IO
+Public Class frmStreamLinker
+    Dim blnValidURL As Boolean = False
+
+    Private Sub frmStreamLinker_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Prompt if the user is attempting to overwrite a file
+        SaveFileDialog1.OverwritePrompt = True
+
+        'Setup the default file extension, and save dialog filter
+        SaveFileDialog1.DefaultExt = ".strm"
+
+        SaveFileDialog1.Filter = "Kodi Compatabile Stream Files (*.strm)|*.strm|All Files|*.*"
+    End Sub
+
+    Private Sub cmdCreateStream_Click(sender As Object, e As EventArgs) Handles cmdCreateStream.Click
+        Dim strStreamURL As String = txtStreamURL.Text
+        Dim strFileLocation As String
+        Dim myOutput As StreamWriter
+
+        'Determine if the URL the user entered is valid and store that as
+        'blnValidURL
+        blnValidURL = Uri.IsWellFormedUriString(strStreamURL, UriKind.Absolute)
+
+        'If the user didn't enter anything or an invalid URL then...
+        If txtStreamURL.Text = "" Or Not blnValidURL Then
+            MessageBox.Show("Please enter a valid URL")
+        Else    'Otherwise the user entered a valid URL
+            SaveFileDialog1.ShowDialog()
+            If SaveFileDialog1.FileName = "" Or SaveFileDialog1.FileName.Contains("?") Then   'If the user clicked cancel or didn't provide a valid filename/path
+                MessageBox.Show("You didn't enter a valid filename. Please enter a valid" &
+                                "filename to save to.")
+            Else    'Otherwise the user saved to a valid file location
+                'Take the filename/path from the SaveFileDialog box
+                'and store it as strFileLocation
+                strFileLocation = SaveFileDialog1.FileName
+
+                'Open a new StreamWriter to the filename/path the user specified
+                myOutput = New StreamWriter(strFileLocation)
+
+                'A Kodi stream file only requires a single properly formatted URL to
+                'a media file saved as plain text in the .strm filetype
+                'so take the URL input from the textbox and make that the contents
+                'of this new textfile
+                myOutput.WriteLine(strStreamURL)
+
+                'Close the output stream writer after we are done writing the file
+                myOutput.Close()
+
+                MessageBox.Show("File Successfully created.")
+                clearInput()    'Clear input fields after successful file creation
+            End If
+        End If
+        'Reset everything to nothing/false after writing the file in preparation for a
+        'potential next file the user may write
+        myOutput = Nothing
+        strFileLocation = Nothing
+        strStreamURL = Nothing
+        blnValidURL = False
+    End Sub
+
+    Private Sub clearInput()
+        'Fires when the user clicks the clear button or after a successful 
+        'file creation
+        'Reset the stream URL text box to nothing and
+        'change focus to that text box.
+        txtStreamURL.Text = ""
+        txtStreamURL.Focus()
+    End Sub
+
+    Private Sub cmdClearInput_Click(sender As Object, e As EventArgs) Handles cmdClearInput.Click
+        'When the user clicks on the clear input button
+        clearInput()
+    End Sub
+End Class
